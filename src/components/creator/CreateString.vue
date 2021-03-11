00f29a254f711d4a54512cbb3b6659d21dac4f56
shijir0330 <eastwindshijir@gmail.com>
2021-03-11 14:02:25 +0800

    drag working for now

--- src/components/creator/CreateString.vue
+++ src/components/creator/CreateString.vue
@@ -2,8 +2,7 @@
   <b-card class="mb-2">
     String
     <input type="text" ref="name" :value="value.name" @input="updateValue"/>
-    <b-form-checkbox ref="required" :v-model="value.required" @input="updateValue"
-                     switch :value="true" :unchecked-value="false">required</b-form-checkbox>
+<!--    <b-form-checkbox ref="required" :v-model="value.required" @input="updateValue" switch :value="true" :unchecked-value="false">required</b-form-checkbox>-->
     <input type="number" ref="minLength" :value="value.minLength" @input="updateValue"/>
     <input type="number" ref="maxLength" :value="value.maxLength" @input="updateValue"/>
   </b-card>
@@ -17,7 +16,7 @@ export default {
     updateValue() {
       this.$emit('input', {
         name: this.$refs.name.value,
-        required: this.$refs.required.value,
+        // required: this.$refs.required.value,
         minLength: this.$refs.minLength.value,
         maxLength: this.$refs.maxLength.value,
       })
--- src/components/creator/DynamicFormCreator.vue
+++ src/components/creator/DynamicFormCreator.vue
@@ -4,23 +4,36 @@
       <b-col cols="8">
         <b-card header-class="">
           <template #header>
-            <b-form-group label="Form Name" label-cols="4">
-              <b-form-input v-model="formName"></b-form-input>
-            </b-form-group>
+            <b-form inline>
+              <label class="mr-2">Form Name</label>
+              <!--              <b-form-group label="Form Name" class="mr-2">-->
+              <b-form-input v-model="formName" class="mr-2"></b-form-input>
+              <!--              </b-form-group>-->
+              <!--              <label class="mr-2">Column number</label>-->
+              <!--              <b-form-group label="Column number" class="mr-2">-->
+              <!--                <b-form-input v-model="formColumn" type="number"></b-form-input>-->
+              <!--              </b-form-group>-->
+            </b-form>
           </template>
           <div v-for="(component, index) in properties" v-bind:key="index">
-            <create-string v-model="component.object" v-if="component.type === 'string'"/>
-            <create-number v-model="component.object" v-if="component.type === 'number'"/>
+            <div @drop="onDrop($event, index)" @dragenter.prevent @dragover.prevent="changeStyle" :style="style"/>
+<!--            <div @dragstart="startDrag($event, component)">-->
+              <create-string v-model="component.object" v-if="component.type === 'string'"/>
+              <!--            <create-number v-model="component.object" v-if="component.type === 'number'"/>-->
+<!--            </div>-->
           </div>
+          <div @drop="onDrop($event, properties.length)" @dragenter.prevent @dragover.prevent
+               style="height: 10px; background-color: red; border-width: 2px; border-style: solid; border-color: black"/>
         </b-card>
       </b-col>
       <b-col cols="4">
         <b-card header="Components">
-          <b-button v-on:click="add('string', {name: null, minLength: null, maxLength: null})" block>String</b-button>
-          <b-button
-              v-on:click="add('number', {name: null, minLength: null, maxLength: null, minValue: null, maxValue: null})"
-              block>Number
-          </b-button>
+          <div v-for="item in components"
+               :draggable="true"
+               @dragstart="startDrag($event, item)"
+               v-on:click="add(item.type, item.properties)">
+            String
+          </div>
         </b-card>
       </b-col>
     </b-row>
@@ -28,7 +41,7 @@
       <b-col>
         <b-button v-on:click="createJson()">Create Json</b-button>
         <b-card>
-          {{schema}}
+          {{ schema }}
         </b-card>
       </b-col>
     </b-row>
@@ -47,20 +60,38 @@ export default {
   },
   data() {
     return {
+      components: [
+        {type: 'string', properties: {name: null, minLength: null, maxLength: null, required: false}}
+      ],
+      style: 'height: 10px; background-color: blue',
       formName: '',
+      formColumn: 1,
       properties: [],
       schema: null
     }
   },
   methods: {
+    startDrag(event, item) {
+      event.dataTransfer.dropEffect = 'move'
+      event.dataTransfer.effectAllowed = 'move'
+      event.dataTransfer.setData('itemType', item.type);
+    },
+    onDrop(event, index) {
+      const type = event.dataTransfer.getData('itemType')
+      const item = this.components.find((item) => item.type === type);
+      this.properties.splice(index, 0, {type: item.type, object: item.properties});
+    },
+    changeStyle() {
+      this.style = 'border-width: 2px; border-style: solid; border-color: black'
+    },
     add(type, object) {
       this.properties.push({type: type, object: object});
     },
     createJson() {
-      let schemaFormat = {name: this.formName, object: {}};
+      let schemaFormat = {name: this.formName, properties: {}};
       this.properties.forEach((x) => {
         const {name, ...others} = x.object;
-        schemaFormat.object[name] = {type: x.type, ...others};
+        schemaFormat.properties[name] = {type: x.type, ...others};
       })
       this.schema = schemaFormat;
     }
