dc6ece35a7bb822ac89312da4909a13494ccf99d
shijir0330 <66103947+shijir0330@users.noreply.github.com>
2021-03-13 18:28:29 +0800

    ?

--- src/components/creator/CreateString.vue
+++ src/components/creator/CreateString.vue
@@ -1,17 +1,17 @@
 <template>
   <div class="padding">
     <template v-if="!isEdit">{{ value.name }}: string</template>
-<!--    <select ref="columns" :value="value.columns" @change="updateValue">-->
-<!--      <option v-for="i in 12" :value="i">{{ i }}</option>-->
-<!--    </select>-->
+    <!--    <select ref="columns" :value="value.columns" @change="updateValue">-->
+    <!--      <option v-for="i in 12" :value="i">{{ i }}</option>-->
+    <!--    </select>-->
     <input :hidden="!isEdit" v-model="value.name"/>
     <input :hidden="!isValidation" type="checkbox" v-model="value.required"/>
     <input :hidden="!isValidation" v-model="value.minLength"/>
     <input :hidden="!isValidation" v-model="value.maxLength"/>
-<!--    <input type="text" :hidden="!isEdit" ref="name" :value="value.name" @input="updateValue"/>-->
-<!--    <input type="checkbox" :hidden="!isValidation" v-model="value.required" :disabled="!value.name">-->
-<!--    <input type="number" :hidden="!isValidation" ref="minLength" :value="value.minLength" @input="updateValue"/>-->
-<!--    <input ref="maxLength" :hidden="!isValidation" :value="value.maxLength" type="number" @input="updateValue"/>-->
+    <!--    <input type="text" :hidden="!isEdit" ref="name" :value="value.name" @input="updateValue"/>-->
+    <!--    <input type="checkbox" :hidden="!isValidation" v-model="value.required" :disabled="!value.name">-->
+    <!--    <input type="number" :hidden="!isValidation" ref="minLength" :value="value.minLength" @input="updateValue"/>-->
+    <!--    <input ref="maxLength" :hidden="!isValidation" :value="value.maxLength" type="number" @input="updateValue"/>-->
     <slot></slot>
   </div>
 </template>
@@ -26,16 +26,16 @@ export default {
   //   }
   // },
   // methods: {
-    // updateValue() {
-    //   this.$emit('input', {
-    //     name: this.$refs.name.value,
-    //     required: this.value.required,
-    //     columns: this.value.columns,
-    //     // columns: this.$refs.columns.value,
-    //     minLength: this.$refs.minLength.value,
-    //     maxLength: this.$refs.maxLength.value,
-    //   })
-    // }
+  //   updateValue() {
+  //     this.$emit('input', {
+  //       name: this.$refs.name.value,
+  //       required: this.value.required,
+  //       columns: this.value.columns,
+  //       // columns: this.$refs.columns.value,
+  //       minLength: this.$refs.minLength.value,
+  //       maxLength: this.$refs.maxLength.value,
+  //     })
+  //   }
   // }
 }
 </script>
