8fe3155736ea57959a265d55ce7ab74a399b6cbd
shijir0330 <66103947+shijir0330@users.noreply.github.com>
2021-03-24 13:34:57 +0800

    ok

--- src/components/DynamicFormCreator.vue
+++ src/components/DynamicFormCreator.vue
@@ -52,17 +52,17 @@
               NO PROPERTY FOUND
             </b-col>
           </b-row>
+          <b-row>
+            <b-col>
+              <b-button variant="primary" v-on:click="submitFormSchema">SUBMIT</b-button>
+            </b-col>
+          </b-row>
         </b-card>
       </b-col>
       <b-col cols="5">
         <b-card header="JSON SCHEMA">
           <pre>{{ value | jsonFormat }}</pre>
         </b-card>
-      </b-col>
-    </b-row>
-    <b-row>
-      <b-col>
-        <b-button variant="primary" v-on:click="submitFormSchema">SUBMIT</b-button>
       </b-col>
     </b-row>
   </div>
@@ -130,7 +130,8 @@ export default {
           const objectArray = Object.entries(x);
           const object = {};
           objectArray.forEach(([key, value]) => {
-            if (value !== null) object[key] = value
+            if (key === 'required') object[key] = value
+            if (value) object[key] = value
           });
           object.validation = undefined;
           schemaFormat.properties.push(object);
@@ -161,7 +162,8 @@ export default {
           const objectArray = Object.entries(others);
           const object = {};
           objectArray.forEach(([key, value]) => {
-            if (value !== null) object[key] = value
+            if (key === 'required') object[key] = value
+            if (value) object[key] = value
           });
           schemaFormat.properties[name] = {...object};
         })
